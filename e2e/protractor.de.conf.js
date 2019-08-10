c1766dc6343bd6ee9e92721ff8680eebfa55d053
Emanuel Hein <github@e-hein.de>
2019-08-10 14:24:22 +0200

    basic structure and stylings

---
+++ e2e/protractor.de.conf.js
@@ -0,0 +1,48 @@
+// @ts-check
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+const { SpecReporter } = require('jasmine-spec-reporter');
+const { join } = require('path');
+
+/**
+ * @type { import("protractor").Config }
+ */
+exports.config = {
+  allScriptsTimeout: 11000,
+  specs: [
+    './src/**/*.e2e-spec.ts'
+  ],
+  capabilities: {
+    'browserName': 'chrome',
+    'logName': 'de'
+  },
+  directConnect: true,
+  baseUrl: 'http://localhost:8080/ssr-de',
+  framework: 'jasmine',
+  plugins: [
+    {
+        // The module name
+        package: 'protractor-image-comparison',
+        // Some options, see the docs for more
+        options: {
+            baselineFolder: join(process.cwd(), './e2e/spec-shots/baseline/'),
+            formatImageName: `{tag}-{logName}-{width}x{height}`,
+            screenshotPath: join(process.cwd(), './e2e/spec-shots/'),
+            savePerInstance: true,
+            // ... more options
+        },
+    },
+  ],
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function() {}
+  },
+  onPrepare() {
+    require('ts-node').register({
+      project: require('path').join(__dirname, './tsconfig.json')
+    });
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  }
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'ng-spec-shot-reviewer';
-}
+export class AppComponent {}
