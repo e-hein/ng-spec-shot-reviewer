3bed13f55a98d23fd41e03723072b4b80e99f9f4
Emanuel Hein <github@e-hein.de>
2019-08-11 13:27:59 +0200

    generated review page component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SpecShotListComponent } from './spec-shot-list/spec-shot-list.component';
+import { ReviewPageComponent } from './review-page/review-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    SpecShotListComponent
+    SpecShotListComponent,
+    ReviewPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/review-page/review-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ssr-review-page',
+  templateUrl: './review-page.component.html',
+  styleUrls: ['./review-page.component.sass']
+})
+export class ReviewPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
