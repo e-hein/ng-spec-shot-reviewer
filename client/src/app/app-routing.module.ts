ad8f75f439b35077aac83d962f0e2124b369b5e1
Emanuel Hein <github@e-hein.de>
2019-08-11 13:42:47 +0200

    add review page to navigation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ReviewPageComponent } from './review-page/review-page.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
+  },
+  {
+    path: 'review',
+    component: ReviewPageComponent,
   }
 ];
 
