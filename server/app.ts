1f53c5fe472a47bbaad704b8ee755ea40a88a7b2
Emanuel Hein <github@e-hein.de>
2019-08-12 10:10:50 +0200

    serve own frontend

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,28 @@
 import * as express from 'express';
 import * as fs from 'fs';
 import chalk from 'chalk';
-import { resolve } from 'path';
+import { resolve, join } from 'path';
 import { SsrEndpoint } from './endpoint';
 import { SsrAppConfig } from './app-config.model';
 
 const app = express();
 const cfg = new SsrAppConfig(process.cwd());
 
-console.log(chalk.green('\nstarting spec shot reviewer ...\n'));
+console.log(chalk.green('\nstarting spec shot reviewer ...'));
+
+const ownFrontends = join(__dirname, '..', 'frontend');
+console.log('\nown frontends in ', ownFrontends);
+fs.readdirSync(ownFrontends).forEach((distFolder) => {
+  const resourceFolder = '/' + distFolder;
+  const targetFolder = resolve(ownFrontends, distFolder);
+  console.log(chalk.yellow(resourceFolder), 'serve static', ':', targetFolder);
+  app.use(resourceFolder, express.static(targetFolder), (req, res, next) => {
+    res.sendFile(targetFolder + '/index.html');
+  });
+});
 
 if (cfg.angularAppsFolder) {
+  console.log('\nother angular apps from', cfg.angularAppsFolder);
   fs.readdirSync(cfg.angularAppsFolder).forEach((distFolder) => {
     const resourceFolder = '/' + distFolder;
     const alreadyUsed = cfg.reservedPathes.some((path) => path.startsWith(resourceFolder));
@@ -25,6 +37,7 @@ if (cfg.angularAppsFolder) {
   });
 }
 
+console.log('\ncore services');
 if (cfg.testPageUrl) {
   console.log(chalk.yellow(cfg.testPageUrl), 'server is alive test page', );
   app.get(cfg.testPageUrl, (_req, res) => res.send('Ok'));
